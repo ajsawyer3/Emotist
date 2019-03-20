0ecc80846c6656b67d9607c0e58d2f4be4ba40e9
Andrew Sawyer <ajsawyer3@gmail.com>
2019-03-19 22:18:44 -0400

    working keypresses

--- HarrisOS.playground/Contents.swift
+++ HarrisOS.playground/Contents.swift
@@ -7,8 +7,11 @@ import PlaygroundSupport
 
 class Scene: SCNScene, SCNSceneRendererDelegate {
     
+    
     var sceneView: SCNView = SCNView(frame: CGRect(x: 0, y: 0, width: 400, height: 800))
     var charachters: [HarrisCharachter] = []
+    
+    
     
     override init() {
         super.init()
@@ -81,102 +84,93 @@ class Scene: SCNScene, SCNSceneRendererDelegate {
         
         
         //        test move user object
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
-                        userCharachter.node.runAction(SCNAction.moveBy(x: 15, y: 0, z: 10, duration: 50))
-             for x in 0...99 {
-            self.charachters[x].calculateEmotionLevel(charachters: self.charachters)
-            }
-//            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) {
+//        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+//            userCharachter.node.runAction(SCNAction.moveBy(x: 15, y: 0, z: 10, duration: 50))
+//
+//            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
 //                for x in 0...99 {
 //                    self.charachters[x].calculateEmotionLevel(charachters: self.charachters)
 //                }
-//                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) {
+//                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
 //                    for x in 0...99 {
 //                        self.charachters[x].calculateEmotionLevel(charachters: self.charachters)
 //                    }
-//                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) {
-//                        userCharachter.happinessLevel = 0.2
+//                    userCharachter.happinessLevel = 1
+//                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
 //                        for x in 0...99 {
 //                            self.charachters[x].calculateEmotionLevel(charachters: self.charachters)
-//                        }
-//                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) {
-//                            for x in 0...99 {
-//                                self.charachters[x].calculateEmotionLevel(charachters: self.charachters)
-//                            }
 //                        }
 //                    }
 //                }
 //            }
-            
-        }
-        
-        
-        //lighting
-        let environment = NSImage(named: "hdri.jpg")
-        self.lightingEnvironment.contents = environment
-        self.lightingEnvironment.intensity = 2.0
-        
-        self.background.contents = NSColor.black
-    }
+//        }
+
+    //lighting
+    let environment = NSImage(named: "hdri.jpg")
+    self.lightingEnvironment.contents = environment
+    self.lightingEnvironment.intensity = 2.0
     
-    func createCameraNode(following target: SCNNode) -> SCNNode {
-        let camera = SCNCamera()
-        
-        camera.focalLength = 24
-        camera.focusDistance = 8.124
-        
-        camera.zFar = 1000
-        camera.zNear = 0
-        
-        camera.fStop = 0.009
-        camera.apertureBladeCount = 5
-        camera.wantsDepthOfField = false
-        
-        camera.bloomBlurRadius = 8
-        camera.bloomIntensity = 0.2
-        camera.bloomThreshold = 0.3
-        
-        camera.vignettingIntensity = 0.3
-        camera.vignettingPower = 1
-        
-        camera.wantsHDR = true
-        
-        let cameraNode = SCNNode()
-        cameraNode.camera = camera
-        
-        
-        
-        let lookAtConstraint = SCNLookAtConstraint(target: target)
-        lookAtConstraint.isGimbalLockEnabled = true
-        
-        let replicateConstraint = SCNReplicatorConstraint(target: target)
-        replicateConstraint.replicatesScale = false
-        replicateConstraint.replicatesOrientation = false
-        
-        replicateConstraint.positionOffset = SCNVector3(x: -5, y: 4, z: 5)
-        
-        let looseFollowConstraint = SCNAccelerationConstraint()
-        
-        
-        cameraNode.constraints = [replicateConstraint, lookAtConstraint, looseFollowConstraint]
-        
-        return cameraNode
-    }
+    self.background.contents = NSColor.black
+}
+
+func createCameraNode(following target: SCNNode) -> SCNNode {
+    let camera = SCNCamera()
+    
+    camera.focalLength = 24
+    camera.focusDistance = 8.124
+    
+    camera.zFar = 1000
+    camera.zNear = 0
+    
+    camera.fStop = 0.009
+    camera.apertureBladeCount = 5
+    camera.wantsDepthOfField = false
+    
+    camera.bloomBlurRadius = 8
+    camera.bloomIntensity = 0.2
+    camera.bloomThreshold = 0.3
+    
+    camera.vignettingIntensity = 0.3
+    camera.vignettingPower = 1
+    
+    camera.wantsHDR = true
+    
+    let cameraNode = SCNNode()
+    cameraNode.camera = camera
     
     
-    //    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-    //
-    //    }
+    
+    let lookAtConstraint = SCNLookAtConstraint(target: target)
+    lookAtConstraint.isGimbalLockEnabled = true
+    
+    let replicateConstraint = SCNReplicatorConstraint(target: target)
+    replicateConstraint.replicatesScale = false
+    replicateConstraint.replicatesOrientation = false
+    
+    replicateConstraint.positionOffset = SCNVector3(x: -5, y: 4, z: 5)
+    
+    let looseFollowConstraint = SCNAccelerationConstraint()
     
     
-    //    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+    cameraNode.constraints = [replicateConstraint, lookAtConstraint, looseFollowConstraint]
     
-    //    }
-    
+    return cameraNode
+}
+
+
+
+
+
+//    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+
+//    }
+
     //idk what this is
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
 }
 
 class HarrisCharachter {
@@ -184,7 +178,7 @@ class HarrisCharachter {
     //    var lightNode: SCNNode
     
     var happinessLevel = Double.random(in: 0...1)
-//    var happinessLevel = 0.5
+    //    var happinessLevel = 0.5
     
     private let light = SCNLight()
     
@@ -223,32 +217,35 @@ class HarrisCharachter {
     func calculateEmotionLevel(charachters: [HarrisCharachter]) {
         var happinessSum = 0.0
         var nearbyCharachterCount = 0.0
+//        for x in 1...(charachters.count-1) {
         for charachter in charachters {
+//            let charachter = charachters[x]
             let selfPostion = SCNVector3ToGLKVector3(self.node.worldPosition)
             let charachterPosition = SCNVector3ToGLKVector3(charachter.node.worldPosition)
             
-            var distance = Double(GLKVector3Distance(selfPostion, charachterPosition))
+            let distance = Double(GLKVector3Distance(selfPostion, charachterPosition))
+            let correctDistance = (distance/21.25)
             
-            var correctDistance = (distance/21.25)
-            if correctDistance < 0.1 {
+            if correctDistance < 0.05 {
                 happinessSum += (1-correctDistance) * charachter.happinessLevel
                 nearbyCharachterCount += 1
             } else {
                 happinessSum += 0
             }
         }
-        //        print("HAPINESS AVG: \(happinessAvg), nearby cc: \(nearbyCharachterCount)")
+        
         
         happinessLevel = happinessSum/nearbyCharachterCount
+         print("HAPINESS AVG: \(happinessLevel), nearby cc: \(nearbyCharachterCount)")
         let newHue = CGFloat(happinessLevel*0.15)
-
+        
         let newColor = NSColor(hue: newHue, saturation: 1, brightness: 1, alpha: 1)
-//        changeColorTo(newColor)
-        SCNTransaction.begin()
-        SCNTransaction.animationDuration = 0.5
+        //        changeColorTo(newColor)
+//        SCNTransaction.begin()
+//        SCNTransaction.animationDuration = 0.5
         node.geometry?.firstMaterial?.diffuse.contents = newColor
         //                lightNode.light?.color = newColor
-        SCNTransaction.commit()
+//        SCNTransaction.commit()
     }
     
     func changeColorTo(_ newColor: NSColor) {
@@ -261,7 +258,46 @@ class HarrisCharachter {
     }
 }
 
+class SceneViewController: NSViewController {
+    private var scene = Scene()
+    
+    override func loadView() {
+        view = NSView(frame: NSRect(x: 0, y: 0, width: 400, height: 800))
+        
+        self.view.addSubview(scene.sceneView)
+        
+        scene.sceneView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scene.sceneView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scene.sceneView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scene.sceneView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scene.sceneView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+            ])
+    }
+    
+    
+    
+    override func keyUp(with event: NSEvent) {
+        let leftArrow: UInt16 = 0x7B
+        let rightArrow: UInt16 = 0x7C
+        let downArrow: UInt16 = 0x7D
+        let upArrow: UInt16 = 0x7E
+
+        if event.keyCode == downArrow {
+            print("DOWN")
+        } else if event.keyCode == upArrow {
+            print("UP")
+        } else if event.keyCode == rightArrow {
+            print("RIGHT")
+        } else if event.keyCode == leftArrow {
+            print("LEFT")
+        } else {
+            print("YOOO")
+        }
+    }
+}
 
 //create scene & add to playground liveview
-let testScene = Scene()
-PlaygroundPage.current.liveView = testScene.sceneView
+let vc = SceneViewController()
+PlaygroundPage.current.liveView = vc
+
